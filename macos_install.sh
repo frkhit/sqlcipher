00312ff795839b7d94bf302ebf11af36d5fdf676
frkhit <frkhit@gmail.com>
2022-02-20 17:06:30 +0800

    clean code

--- macos_install.sh
+++ macos_install.sh
@@ -10,9 +10,9 @@ set -x
 # build openssl
 [[ -d build/openssl/ ]] || git clone --branch OpenSSL_1_1_0-stable --depth=1 git@github.com:openssl/openssl.git build/openssl/
 
-#pushd build/openssl/
-#  ./Configure darwin64-x86_64-cc && make
-#popd
+pushd build/openssl/
+  ./Configure darwin64-x86_64-cc && make
+popd
 
 # build sqlcipher
 make clean
